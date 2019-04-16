ffcdb222f4cefd7e5767bc9b09ac16e976e5deaf
Andrey Kropotin <tork713@gmail.com>
2019-04-16 17:27:27 +0700

    16.04.2019 17:27

--- str_methods/str_methods.py
+++ str_methods/str_methods.py
@@ -1,1 +1,7 @@
-
+name = 'Swaroop' #Это объект строки
+if name.startswith('Swa'):
+    print('Да строка начинается на "Swa"')
+if 'a' in name:
+    print('Да строка содержит "a"')
+if True:
+    pass
